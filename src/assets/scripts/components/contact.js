c5cac1431afcad73052bf4ae0c9b03bda9355b8e
Alexander Alarcon <walarcon14@gmail.com>
2020-06-02 12:29:00 -0500

    Render Contact

--- src/assets/scripts/components/contact.js
+++ src/assets/scripts/components/contact.js
@@ -4,11 +4,28 @@ import typedefs from '../typedefs/';
  * Create a contact card
  * @param {!Contact} contact - Contact to be rendered
  */
-function createContactElement(contact) {
-  const pElement = document.createElement('p');
-  pElement.textContent = contact.name;
+function createContactElement(contact, onEdit, onDelete) {
+  const contactTpl = document.getElementById('contact-tpl');
+  const contactElement = contactTpl.content.cloneNode(true).firstElementChild;
+  const name = contactElement.querySelector('.name-group').lastElementChild;
+  const address = contactElement.querySelector('.address-group')
+    .lastElementChild;
+  const phone = contactElement.querySelector('.phone-group').lastElementChild;
+  const buttons = contactElement.querySelector('.actions');
 
-  return pElement;
+  name.textContent = contact.name;
+  address.textContent = contact.address;
+  phone.textContent = contact.phone;
+  contactElement.dataset.id = contact.id;
+  buttons.firstElementChild.addEventListener(
+    'click',
+    onEdit.bind(null, contact.id)
+  );
+  buttons.lastElementChild.addEventListener(
+    'click',
+    onDelete.bind(null, contact.id)
+  );
+  return contactElement;
 }
 
 export default createContactElement;
--- src/assets/scripts/components/contactList.js
+++ src/assets/scripts/components/contactList.js
@@ -1,6 +1,15 @@
 import typedefs from '../typedefs/';
 
 import Contact from './contact';
+
+const editHandler = (id) => {
+  console.log(`Editing contact with id: ${id}`);
+};
+
+const deleteHandler = (id) => {
+  console.log(`Deleting contact with id: ${id}`);
+};
+
 /**
  * Create the contact list
  * @param {!Contact[]} contacts - array of contacts to be rendered
@@ -11,17 +20,21 @@ function createContactListElement(contacts) {
   const contactListElement = contactListTpl.content.cloneNode(true)
     .firstElementChild;
   const fragment = document.createDocumentFragment();
+  const newContactButton = document.createElement('button');
+  newContactButton.textContent = 'Nuevo';
+  newContactButton.className = 'btn btn-primary mt-4';
   if (contacts.length === 0) {
     const pElement = document.createElement('p');
     pElement.textContent = 'No Contacts';
     fragment.appendChild(pElement);
   } else {
     for (const contact of contacts) {
-      const contactEl = Contact(contact);
+      const contactEl = Contact(contact, editHandler, deleteHandler);
       fragment.appendChild(contactEl);
     }
   }
   contactListElement.appendChild(fragment);
+  contactListElement.appendChild(newContactButton);
   return contactListElement;
 }
 
